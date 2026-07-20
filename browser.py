7ce3738d9741d6f0c7595e678ce831438f747066
Weri Oliveira <werioliveira@hotmail.com>
2026-07-19 23:00:00 -0300

    fix render windows on headless

--- browser.py
+++ browser.py
@@ -2,6 +2,7 @@ import platform
 import logging
 import os
 from DrissionPage import ChromiumPage, ChromiumOptions
+from requests import options
 
 logger = logging.getLogger("cloudflare-bypass.browser")
 
@@ -14,9 +15,6 @@ def create_browser(proxy: str = None, headless: bool = False, instance_id: str =
     options.set_argument("--no-sandbox")
     options.set_argument("--disable-setuid-sandbox")
     options.set_argument("--disable-dev-shm-usage") 
-    options.set_argument("--enable-webgl")
-    options.set_argument("--use-gl=angle")
-    options.set_argument("--use-angle=swiftshader")
     
     options.set_argument("--disable-logging")
     options.set_argument("--log-level=3")
@@ -28,19 +26,19 @@ def create_browser(proxy: str = None, headless: bool = False, instance_id: str =
     # --- 2. CONFIGURAÇÃO DE SISTEMA OPERACIONAL ---
     if is_windows:
         logger.info("Executando no Windows")
-        options.set_argument("--force-renderer-accessibility") 
-        options.set_argument("--no-default-browser-check")
-        options.headless(False)
+        options.set_user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+        options.set_argument("--window-size=1920,1080")
+        options.headless(True)
         options.auto_port()
     else:
         logger.info("Executando no Linux (Docker/WSL)")
-        
+        options.set_user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
         browser_path = os.getenv("CHROMIUM_PATH", "/usr/bin/google-chrome-stable")
         options.set_paths(browser_path=browser_path)
         
         # CORREÇÃO 1: Usar auto_port() para evitar colisão de portas ao recriar navegadores
         options.auto_port() 
-        
+        options.set_argument("--window-size=1920,1080")
         options.set_argument("--headless=new")
         options.set_argument("--accept-lang=en-US")
         
